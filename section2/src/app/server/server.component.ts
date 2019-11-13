6ae0dceae631c2ca33bee01246705390f7bffff2
artir3 <markowski.artur@outlook.com>
2019-11-13 21:17:50 +0100

    after lection 33

--- section2/src/app/server/server.component.ts
+++ section2/src/app/server/server.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
     styleUrls: ['./server.component.css']
   })
-  export class ServerComponent {
+  export class ServerComponent implements OnInit {
+
+    ngOnInit(): void { }
   }
   
