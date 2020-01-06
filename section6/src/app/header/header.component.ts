d688b9c6147c4c0938c855ab627f9e28aa9f7497
artir3 <markowski.artur@outlook.com>
2020-01-06 12:47:07 +0100

    section 5 changes

--- section6/src/app/header/header.component.ts
+++ section6/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  @Output() selectedElement = new EventEmitter<string>();
+  onSelected(element: string) {
+    this.selectedElement.emit(element);
+  }
 }
