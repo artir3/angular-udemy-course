b0e1b34d7c2f2fbec3a7e362331cfd458eb0d017
artir3 <markowski.artur@outlook.com>
2020-01-06 19:35:06 +0100

    after lesson 94

---
+++ section7/src/app/more-universal-directive/better-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+}
