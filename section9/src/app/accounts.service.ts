7d22a7f4c2d3faa5ac1a1fbe96e5874426393ba4
artir3 <markowski.artur@outlook.com>
2020-01-06 21:11:05 +0100

    after lesson 111 - move logger from components to service

--- section9/src/app/accounts.service.ts
+++ section9/src/app/accounts.service.ts
@@ -1,29 +1,36 @@
+import { LoggingService } from "./logging/logging.service";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AccountsService {
-    private accounts = [
-        {
-          name: 'Master Account',
-          status: 'active'
-        },
-        {
-          name: 'Testaccount',
-          status: 'inactive'
-        },
-        {
-          name: 'Hidden Account',
-          status: 'unknown'
-        }
-      ];
-
-      addAccount(name: string, status:  string) { 
-          this.accounts.push({name: name, status: status})
-      }
-      
-      updateStatus(id: number, status:  string) { 
-          this.accounts[id].status = status;
-      }
-
-      getAccounts() {
-        return this.accounts;
-      }
+  private accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
     }
+  ];
+
+  constructor(private logger: LoggingService) { }
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name: name, status: status })
+    this.logger.logStatusChange(status);
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+    this.logger.logStatusChange(status);
+  }
+
+  getAccounts() {
+    return this.accounts;
+  }
+}
