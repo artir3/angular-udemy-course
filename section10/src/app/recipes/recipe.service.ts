af51bd6d62c460414a7ad9600555204450bfce30
artir3 <markowski.artur@outlook.com>
2020-01-07 00:04:44 +0100

    s10e121 after added ingredients to recipe

--- section10/src/app/recipes/recipe.service.ts
+++ section10/src/app/recipes/recipe.service.ts
@@ -1,13 +1,22 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 // @Injectable({
 //   providedIn: 'root'
 // })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Tasty Schnitzel', 'A super tasty schnitzel', 'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Frise', 20)
+      ]),
+    new Recipe('Big Fat Burger', 'What else you need to say?', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/570px-RedDot_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -15,7 +24,7 @@ export class RecipeService {
   constructor() { }
 
   getRecipes() {
-    return this.recipes.slice(); 
+    return this.recipes.slice();
     //this return only copy of list, no reference
   }
 }
