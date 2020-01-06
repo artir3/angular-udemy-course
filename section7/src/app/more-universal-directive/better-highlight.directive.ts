8190371aa319e6b78fa7e54db7c8c66b78703543
artir3 <markowski.artur@outlook.com>
2020-01-06 19:47:42 +0100

    after lection 96 added reactive directive - HostBinding and HostListener

--- section7/src/app/more-universal-directive/better-highlight.directive.ts
+++ section7/src/app/more-universal-directive/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -6,6 +6,8 @@ import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular
 export class BetterHighlightDirective
 //  implements OnInit 
 {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transaprent';
+  
   constructor(private renderer: Renderer2, private elRef: ElementRef) { }
   // ngOnInit(): void {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
@@ -13,12 +15,15 @@ export class BetterHighlightDirective
   // }
   
   @HostListener('mouseenter') mouseOver(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
   }
+
 }
