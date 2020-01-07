f149da3f4a06e6073268950cae9c0344b2d111d8
artir3 <markowski.artur@outlook.com>
2020-01-07 23:28:42 +0100

    after s11e144 - move routering configuration to own file app-routing.module

---
+++ section11/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./users/user/user.component";
+import { ServersComponent } from "./servers/servers.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NgModule } from "@angular/core";
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'users', component: UsersComponent, children: [
+            { path: ':id/:name', component: UserComponent }, //node parameters
+        ]
+    },
+    {
+        path: 'servers', component: ServersComponent, children: [ //nested routing
+            { path: ':id', component: ServerComponent },
+            { path: ':id/edit', component: EditServerComponent } //query parameters
+        ]
+    },
+    { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' } //it have to be last element, because order is very important
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
