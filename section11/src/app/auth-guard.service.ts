4ad51f56ff8699c40624e2f61542cf420719bd2f
artir3 <markowski.artur@outlook.com>
2020-01-08 00:19:15 +0100

    after s11e147 - guard -> added fake auth guard with canActivate on servers

---
+++ section11/src/app/auth-guard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.authService.isAuthenticated().then((auth: boolean) => {
+            if (auth) return true;
+            else this.router.navigate(['/'])
+        })
+    }
+}
