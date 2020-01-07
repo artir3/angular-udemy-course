bae264f8bc90949a262a29f9cd5549c9a63b6ab2
artir3 <markowski.artur@outlook.com>
2020-01-07 22:04:34 +0100

    after s11e133 active routing with parameters

--- section11/src/app/app.module.ts
+++ section11/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'users', component: UsersComponent },
   { path: 'servers', component: ServersComponent }
 ]
