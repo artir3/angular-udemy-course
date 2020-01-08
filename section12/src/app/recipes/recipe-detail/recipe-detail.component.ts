d1a171367eb3b7c939a18996f416aec96b2f7819
artir3 <markowski.artur@outlook.com>
2020-01-08 03:53:12 +0100

    after s12e163 return working routing in recipe list

--- section12/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ section12/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,10 +9,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
   constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
-
+  id: number;
+  
   ngOnInit() {
+    this.route.params.subscribe((params:Params)=> {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   onAddToShoppingList() {
