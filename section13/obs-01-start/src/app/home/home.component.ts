61bf34b85ae44b2999744709d1a9d67ae7512779
artir3 <markowski.artur@outlook.com>
2020-01-08 22:26:20 +0100

    s13e175 observers -> map, filter etc

--- section13/obs-01-start/src/app/home/home.component.ts
+++ section13/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators' 
 
 @Component({
   selector: 'app-home',
@@ -14,21 +15,24 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSubscription = interval(1000).subscribe(next => {
     //   console.log(next?3:9)
     // })
-    const customIntervalObservable = Observable.create((observer) => {
-      let count = -1;
+    const customIntervalObservable = Observable.create((observer) => { 
+      let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 2 ){
+        if (count === 6 ){
           observer.complete();
 
         }
-        if (count > 3) {
+        if (count > 10) {
           observer.error(new Error('Count >3!'));
         }
         count++;
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(v => {
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter((data: number) => {return data % 2 !==0}),
+      map((data:number) => 'Round ' + (data +1)))
+      .subscribe(v => {
       console.log(v);
     }, error => {
       console.log(error.message);
