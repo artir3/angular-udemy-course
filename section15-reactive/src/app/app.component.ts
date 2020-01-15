51dc8501e7aca7875dd8b11398623c2b60f7cb9c
artir3 <markowski.artur@outlook.com>
2020-01-15 01:47:46 +0100

    after s15e209 arrays of form Controls (form control)

--- section15-reactive/src/app/app.component.ts
+++ section15-reactive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +19,21 @@ export class AppComponent implements OnInit {
           Validators.email
         ])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     })
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
   }
 }
