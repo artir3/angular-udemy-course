33140a5ebe0942152b95243028fd1fcb1b382c54
artir3 <markowski.artur@outlook.com>
2020-01-15 03:17:13 +0100

    after section 15 practise 7

--- section15-practical-7/src/app/app.component.ts
+++ section15-practical-7/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  signupForm: FormGroup;
+  // projectStatuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'name': new FormControl(null, [
+        Validators.required,
+        CustomValidators.invalidProjectName
+      ],
+       CustomValidators.asyncInvalidProjectName
+      ),
+      'email': new FormControl(null, [
+        Validators.email,
+        Validators.required
+      ]),
+      'status': new FormControl('critical')
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value)
+  }
 }
