4724713588116196f9977da79dca0d1d772d1f8e
artir3 <markowski.artur@outlook.com>
2020-01-15 21:18:27 +0100

    after s16e226 create recipe added html Ccomponent and init values

--- section16/src/app/recipes/recipe-added/recipe-added.component.ts
+++ section16/src/app/recipes/recipe-added/recipe-added.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-added',
@@ -9,14 +11,37 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeAddedComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-    })
+      this.initForm();
+    });
   }
 
+  private initForm() {
+    let name = '';
+    let imagePath = '';
+    let description = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      name = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(name),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(description)
+    })
+  }
 }
