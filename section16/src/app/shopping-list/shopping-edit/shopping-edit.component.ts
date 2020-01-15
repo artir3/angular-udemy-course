c0b2a64fb9cb1b4e7085bbb236e7ac29b696540f
artir3 <markowski.artur@outlook.com>
2020-01-15 18:07:27 +0100

    after s16e218 shopping-edit.component changed to simple reactive model

--- section16/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ section16/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,27 +9,21 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
-  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAddItem() {
-    const name = this.nameInput.nativeElement.value;
-    const amount = this.amountInput.nativeElement.value;
-    if (name && amount){
-      const newIngredient = new Ingredient(name, amount);
-      this.shoppingListService.addIngredient(newIngredient);
-    }
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 
   onDeleteItem() {
-    
+
   }
 
   onClearItem() {
-    
+
   }
 }
