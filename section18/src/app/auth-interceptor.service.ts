8b64355e86ace76d9bc92e7c73a19ea33769d421
artir3 <markowski.artur@outlook.com>
2020-01-16 14:50:13 +0100

    after s18e271 added own HTTP interceptor

---
+++ section18/src/app/auth-interceptor.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+} from '@angular/common/http';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        console.log('Request is on its way');
+        return next.handle(req);
+    }
+}
