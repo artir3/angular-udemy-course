9af9a784f5ff8ff108e6eed2f9c995a83aa9c488
artir3 <markowski.artur@outlook.com>
2020-01-16 11:41:05 +0100

    after s17e248 pipes on html -> async pipes

--- section17/src/app/app.component.ts
+++ section17/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    },2000);
+  })
+
   servers = [
     {
       instanceType: 'medium',
