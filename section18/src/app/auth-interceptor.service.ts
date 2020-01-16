16ec52c2bda37390d5042ce97ca44eef0a44597d
artir3 <markowski.artur@outlook.com>
2020-01-16 15:00:27 +0100

    after s18e273 added own HTTP response interceptors

--- section18/src/app/auth-interceptor.service.ts
+++ section18/src/app/auth-interceptor.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpEventType
 } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -11,6 +12,13 @@ export class AuthInterceptor implements HttpInterceptor {
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'test')
         })
-        return next.handle(modifiedRequest);
+        return next.handle(modifiedRequest).pipe(tap(event => {
+            console.log('Inside incerteptor');
+            console.log(event);
+            if(event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data:');
+                console.log(event.body);
+            }
+        }));
     }
 }
