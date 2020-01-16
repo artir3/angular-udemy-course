d6cbbe5551379d56d2b4041e99744b1bc862cb1a
artir3 <markowski.artur@outlook.com>
2020-01-16 14:54:52 +0100

    after s18e272 added own HTTP interceptor - modifying response

--- section18/src/app/auth-interceptor.service.ts
+++ section18/src/app/auth-interceptor.service.ts
@@ -7,6 +7,10 @@ import {
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
-        return next.handle(req);
+        console.log(req.url);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'test')
+        })
+        return next.handle(modifiedRequest);
     }
 }
