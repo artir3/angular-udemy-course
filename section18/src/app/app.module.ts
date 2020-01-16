0c6d960f9e0a18762ff6b1fadd2200456089d176
artir3 <markowski.artur@outlook.com>
2020-01-16 15:14:42 +0100

    after s18e274 multiple interceptors

--- section18/src/app/app.module.ts
+++ section18/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth-interceptor.service';
+import { LoggingInterceptor } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }    
   ],
   bootstrap: [AppComponent]
 })
--- section18/src/app/auth-interceptor.service.ts
+++ section18/src/app/auth-interceptor.service.ts
@@ -1,24 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpEventType
-} from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request is on its way');
-        console.log(req.url);
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'test')
         })
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log('Inside incerteptor');
-            console.log(event);
-            if(event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data:');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 }
