9700d206fbed49789141f4c4624aa50c14864f38
artir3 <markowski.artur@outlook.com>
2020-01-30 21:41:38 +0100

    after s22e332 lazy loading added auth, shopping-list

--- section22/src/app/app-routing.module.ts
+++ section22/src/app/app-routing.module.ts
@@ -9,6 +9,14 @@ const routes: Routes = [
     // loadChildren: './recipes/recipes.module#RecipesModule'
     // below is new we where we need tsconfig.json -> "module": "esnext"
     loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: 'shoppinglist',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ]
 
