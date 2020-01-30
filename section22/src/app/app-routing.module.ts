87f5b0cd56a9d0d685e67a7cecc6fd1d81708444
artir3 <markowski.artur@outlook.com>
2020-01-30 01:07:23 +0100

    after s22e328 created AUTH MODULE

--- section22/src/app/app-routing.module.ts
+++ section22/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth/auth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent }
 ]
 
 @NgModule({
