e68bf61b99cc1cd9249c60e03069bc7462f2fd72
artir3 <markowski.artur@outlook.com>
2020-02-02 17:55:19 +0100

    after s24e355 restyle

--- section24/src/app/shopping-list/store/shopping-list.reducer.ts
+++ section24/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -11,40 +11,40 @@ const initialState = {
 };
 
 export function shoppingLitReducer(
-  state = initialState, 
+  state = initialState,
   action: ShoppingListActions.ShoppingListActions
-  ) {
+) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state, ingredients: [...state.ingredients, action.payload]
       };
-      case ShoppingListActions.ADD_INGREDIENTS: {
-        return {
-          ...state, ingredients: [...state.ingredients, ...action.payload]
-        };
+    case ShoppingListActions.ADD_INGREDIENTS: {
+      return {
+        ...state, ingredients: [...state.ingredients, ...action.payload]
+      };
+    }
+    case ShoppingListActions.UPDATE_INGREDIENTS: {
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        // we copy whole old object, then we override only that variables what we need.
+        // So if we have id in old object we keep it, but we override other variables
+        ...ingredient, ...action.payload.ingredient
       }
-      case ShoppingListActions.UPDATE_INGREDIENTS: {
-        const ingredient = state.ingredients[action.payload.index];
-        const updatedIngredient = {
-          // we copy whole old object, then we override only that variables what we need.
-          // So if we have id in old object we keep it, but we override other variables
-          ...ingredient, ...action.payload.ingredient
-        }
-        const updatedIngredients = [...state.ingredients];
-        updatedIngredients[action.payload.index] = updatedIngredient;
-        return {
-          ...state, ingredients: updatedIngredients
-        };
-      }
-      case ShoppingListActions.DELETE_INGREDIENTS: {
-        return {
-          ...state, ingredients: state.ingredients.filter((ig, index) => {
-            return index !== action.payload;
-          })
-        };
-      }
-      default: 
-        return state;
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+      return {
+        ...state, ingredients: updatedIngredients
+      };
+    }
+    case ShoppingListActions.DELETE_INGREDIENTS: {
+      return {
+        ...state, ingredients: state.ingredients.filter((ig, index) => {
+          return index !== action.payload;
+        })
+      };
+    }
+    default:
+      return state;
   }
 }
