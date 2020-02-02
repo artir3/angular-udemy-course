3b83cfe9ffbed6c9621df8a172f76c78a60fe0c4
artir3 <markowski.artur@outlook.com>
2020-02-02 23:04:11 +0100

    after s24e375 autologout after some seconds - setTimeout with store logic

--- section24/src/app/auth/auth.service.ts
+++ section24/src/app/auth/auth.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject, BehaviorSubject } from 'rxjs';
-import { AuthResponseData } from './auth-response-data.model';
-import { User } from './user.model';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
