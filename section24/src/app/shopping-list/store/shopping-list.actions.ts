0f8a072bcba7f660416a474ff6f594068e22ecb9
artir3 <markowski.artur@outlook.com>
2020-02-02 17:50:40 +0100

    after s24e355 added upadate and delete to shopping-list.store

--- section24/src/app/shopping-list/store/shopping-list.actions.ts
+++ section24/src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,20 +1,34 @@
 import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/models/ingredient.model";
-import { Ingredients } from "src/app/shared/models/ingredients.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
 
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
-    constructor(public ingredient: Ingredient) {}
+    constructor(public payload: Ingredient) { }
 }
 
 export class AddIngredients implements Action {
     readonly type = ADD_INGREDIENTS;
-    constructor(public ingredients: Ingredient[]) {}
+    constructor(public payload: Ingredient[]) { }
 }
 
-export type ShoppingListActions = 
+export class UpdateIngredients implements Action {
+    readonly type = UPDATE_INGREDIENTS;
+    constructor(public payload: { index: number, ingredient: Ingredient }) { }
+}
+
+export class DeleteIngredients implements Action {
+    readonly type = DELETE_INGREDIENTS;
+    constructor(public payload: number) { }
+}
+
+export type ShoppingListActions =
     AddIngredient |
-    AddIngredients;
+    AddIngredients |
+    UpdateIngredients |
+    DeleteIngredients
+    ;
