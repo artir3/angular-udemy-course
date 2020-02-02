f366f95e111cd947ae8a6cc0508793a230e06bc4
artir3 <markowski.artur@outlook.com>
2020-02-02 22:01:49 +0100

    after s24e371 added signup actions and rename login to authenticateSuccess and loginFailed to authenticationFailes

--- section24/src/app/auth/auth/auth.component.ts
+++ section24/src/app/auth/auth/auth.component.ts
@@ -52,26 +52,12 @@ export class AuthComponent implements OnInit, OnDestroy {
 
       const email = form.value.email;
       const password = form.value.password;
-
-      let authObs: Observable<AuthResponseData>;
-
+      const user = { email: email, password: password };
       if (this.isLoginMode) {
-        this.store.dispatch(new AuthActions.LoginStart({ email: email, password: password }))
-        // authObs = this.authService.login(email, password)
+        this.store.dispatch(new AuthActions.LoginStart(user))
       } else {
-        authObs = this.authService.signUp(email, password)
+        this.store.dispatch(new AuthActions.SignupStart(user))
       }
-
-
-
-      // authObs.subscribe(response => {
-      //   this.isLoading = false;
-      //   this.router.navigate(['/recipes']);
-      // }, errorMeessage => {
-      //   this.error = errorMeessage;
-      //   this.showErrorAlert(errorMeessage);
-      //   this.isLoading = false;
-      // });
 
       form.reset();
     } else return;
--- section24/src/app/auth/store/auth.effects.ts
+++ section24/src/app/auth/store/auth.effects.ts
@@ -19,11 +19,60 @@ const headers = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
+const handleAuthentication = (resData: AuthResponseData) => {
+    const expirationDate = new Date(new Date().getTime() + resData.expiresIn + 1000);
+    const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
+    return new AuthActions.AuthenticateSuccess(user);
+}
+
+const handleError = (error: HttpErrorResponse) => {
+    let errorMessage = 'An unknown error occured!'
+    if (error.error && error.error.error) {
+        switch (error.error.error.message) {
+            case 'EMAIL_EXISTS': {
+                errorMessage = 'The email address is already in use by another account';
+                break;
+            }
+            case 'OPERATION_NOT_ALLOWED': {
+                errorMessage = 'Password sign-in is disabled for this project';
+                break;
+            }
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER': {
+                errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+            }
+            case 'EMAIL_NOT_FOUND': {
+                errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted';
+                break;
+            }
+            case 'INVALID_PASSWORD': {
+                errorMessage = 'The password is invalid or the user does not have a password';
+                break;
+            }
+            case 'USER_DISABLED': {
+                errorMessage = 'The user account has been disabled by an administrator';
+                break;
+            }
+        }
+    }
+    return of(new AuthActions.AuthenticateFailed(errorMessage));
+}
+
 @Injectable()
 export class AuthEffects {
     @Effect()
     authSignUp = this.actions$.pipe(
-        ofType(AuthActions.SIGNUP_START)
+        ofType(AuthActions.SIGNUP_START),
+        switchMap((signupAction: AuthActions.SignupStart) => {
+            return this.http.post<AuthResponseData>(
+                environment.signUpUrl,
+                new SignModel(signupAction.payload.email, signupAction.payload.password),
+                headers
+            ).pipe(
+                map(resData => handleAuthentication(resData)),
+                catchError(error => handleError(error)),
+            )
+        })
     );
 
 
@@ -36,15 +85,8 @@ export class AuthEffects {
                 new SignModel(authDate.payload.email, authDate.payload.password),
                 headers
             ).pipe(
-                map((resData: AuthResponseData) => {
-                    const expirationDate = new Date(new Date().getTime() + resData.expiresIn + 1000);
-                    const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
-                    return new AuthActions.AuthenticateSuccess(user);
-                }),
-                catchError(error => {
-                    const handleError = this.handleError(error)
-                    return of(new AuthActions.AuthenticateFailed(handleError));
-                }),
+                map(resData => handleAuthentication(resData)),
+                catchError(error => handleError(error)),
             )
         }),
     );
@@ -63,36 +105,4 @@ export class AuthEffects {
         private router: Router
     ) { }
 
-    private handleError(errorResponse: HttpErrorResponse) {
-        let errorMessage = 'An unknown error occured!'
-        if (errorResponse.error && errorResponse.error.error) {
-            switch (errorResponse.error.error.message) {
-                case 'EMAIL_EXISTS': {
-                    errorMessage = 'The email address is already in use by another account';
-                    break;
-                }
-                case 'OPERATION_NOT_ALLOWED': {
-                    errorMessage = 'Password sign-in is disabled for this project';
-                    break;
-                }
-                case 'TOO_MANY_ATTEMPTS_TRY_LATER': {
-                    errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-                    break;
-                }
-                case 'EMAIL_NOT_FOUND': {
-                    errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted';
-                    break;
-                }
-                case 'INVALID_PASSWORD': {
-                    errorMessage = 'The password is invalid or the user does not have a password';
-                    break;
-                }
-                case 'USER_DISABLED': {
-                    errorMessage = 'The user account has been disabled by an administrator';
-                    break;
-                }
-            }
-        }
-        return errorMessage;
-    }
 }
