abfd49cba69e8ac52bb7cdf7c5a0708f91de6806
artir3 <markowski.artur@outlook.com>
2020-02-02 19:22:19 +0100

    after s24e361 added AuthActions and methods to AuthReducer

--- section24/src/app/auth/store/auth.reducer.ts
+++ section24/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user.model";
+import * as fromAuthActions from "./auth.actions";
 
 export interface State {
     user: User;
@@ -8,6 +9,16 @@ const initialState = {
     user: null
 }
 
-export function authReducer(state = initialState, action) {
-    return state;
+export function authReducer(state = initialState, action: fromAuthActions.AuthActions) {
+    switch (action.type) {
+        case fromAuthActions.LOGIN:
+            return { ...state, user: action.payload }
+        case fromAuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            }
+        default:
+            return state;
+    }
 }
