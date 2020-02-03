df5576adfc1394f0400baa8c41b7880a7befda90
artir3 <markowski.artur@outlook.com>
2020-02-03 01:38:35 +0100

    after s24e379 added most common funciton to reecipe storee

---
+++ section24/src/app/recipes/store/recipe.actions.ts
@@ -0,0 +1,43 @@
+import { Action } from "@ngrx/store";
+import { Recipe } from "../recipe.model";
+
+export const DELETE_RECIPE = '[Recipes] Delete Recipe';
+export const SET_RECIPES = '[Recipes] Set Recipes';
+export const ADD_RECIPE = '[Recipes] Add Recipe';
+export const UPDATE_RECIPE = '[Recipes] Update Recipe';
+export const CLEAR_RECIPES = '[Recipes] Clear Recipes';
+
+export class DeleteRecipe implements Action {
+    readonly type = DELETE_RECIPE;
+    constructor(public payload: number) { }
+}
+
+export class SetRecipes implements Action {
+    readonly type = SET_RECIPES;
+    constructor(public payload: Recipe[]) { }
+}
+
+export class AddRecipe implements Action {
+    readonly type = ADD_RECIPE;
+    constructor(public payload: Recipe) { }
+}
+
+export class UpdateRecipe implements Action {
+    readonly type = UPDATE_RECIPE;
+    constructor(public payload: Recipe) { }
+}
+
+export class ClearRecipes implements Action {
+    readonly type = CLEAR_RECIPES;
+}
+
+export type RecipeActions =
+    | SetRecipes
+    | AddRecipe
+    | UpdateRecipe
+    | DeleteRecipe
+    | ClearRecipes
+    ;
+
+
+
---
+++ section24/src/app/recipes/store/recipe.reducer.ts
@@ -0,0 +1,48 @@
+import { Recipe } from "../recipe.model";
+import * as RecipeActions from './recipe.actions'
+
+
+export interface State {
+    recipes: Recipe[]
+}
+
+const initialState: Recipe[] = [];
+
+export function recipeReducer(
+    state = initialState,
+    action: RecipeActions.RecipeActions
+) {
+    switch (action.type) {
+        case RecipeActions.SET_RECIPES: {
+            return {
+                ...state,
+                recipes: [action.payload]
+            }
+        }
+        case RecipeActions.ADD_RECIPE: {
+            return {
+                ...state,
+            }
+        }
+        case RecipeActions.UPDATE_RECIPE: {
+            return {
+                ...state,
+            }
+        }
+        case RecipeActions.DELETE_RECIPE: {
+            return {
+                ...state,
+            }
+        }
+        case RecipeActions.CLEAR_RECIPES: {
+            return {
+                ...state,
+                recipes: []
+            }
+        }
+        default:
+            return state;
+    }
+
+}
+
