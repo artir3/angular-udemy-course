5b30f3d877a26bf66f0c40088d4d522afbe0d147
artir3 <markowski.artur@outlook.com>
2020-02-08 21:01:38 +0100

    s26e462 added grouping transitions

--- section26/src/app/app.component.ts
+++ section26/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -94,10 +94,16 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
           })
         ]))]),
       transition('* => void', [
-        animate(300), style({
-          opacity: 0,
-          transform: 'translate(100px)'
-        })
+        group([
+          animate(300), style({
+            color: 'red'
+          }),
+          animate(800), style({
+            opacity: 0,
+            transform: 'translate(100px)'
+          }),
+        ])
+
       ]),
     ]),
 
