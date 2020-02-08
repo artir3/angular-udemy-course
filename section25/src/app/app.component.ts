fe9d61c3f98f046525ee0724e099c1f1761d51b4
artir3 <markowski.artur@outlook.com>
2020-02-08 18:18:54 +0100

    s25e440 fix isue with servers and apps

--- section25/src/app/app.component.ts
+++ section25/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { LoggingService } from './logging.service';
 import * as formApp from './store/app.reducer';
 import { Store } from '@ngrx/store';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,14 @@ import * as AuthActions from './auth/store/auth.actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<formApp.AppState>, private logger: LoggingService) { }
+  constructor(
+    private store: Store<formApp.AppState>, 
+    private logger: LoggingService,
+    @Inject(PLATFORM_ID) private platformId) { }
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.logger.message('Here AppComponent ngOnInit');
   }
 }
